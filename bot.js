7357475a8ff24887e247ca97c793d2ceca8532c7
Sviat Dev <matrix.yurinets.sv@gmail.com>
2024-03-13 13:21:15 -0500

    clear

--- bot.js
+++ bot.js
@@ -13,12 +13,8 @@ const dbConfig = {
   database: process.env.DB_NAME,
 };
 
-// Create a pool to handle multiple connections
 const pool = mysql.createPool(dbConfig);
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-const rssFeedUrl = 'https://www.upwork.com/ab/feed/jobs/rss?api_params=1&job_type=fixed&orgUid=732306551933890561&paging=0%3B10&proposals=0-4%2C5-9%2C10-14&q=%28php%20OR%20laravel%20OR%20API%20OR%20developer%20OR%20engineer%20OR%20Back-End%20OR%20Backend%20OR%20Back%29&securityToken=258baeaa9538ed0424704f375d424cd853c0f72d7b4d8f7aa1f1ee75ad35ea4625025a59e2c7a732dfd50260c276966aa405b7fcce2e7d35038cf5bf128ebcbe&sort=recency&subcategory2_uid=531770282584862733&userUid=732306551929696256&verified_payment_only=1';
 
 const menu = Markup.keyboard([
   ['View RSS list', 'Add new RSS'],
